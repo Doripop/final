556e335234f7221230fc9ac74f15da2779e7163a
seongIllee <a01040579861@gmail.com>
2022-06-30 16:25:45 +0900

    메인페이지 스타일 작업

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./header";
+import Banner from "./Banner";
+import Search from "./Search";
+import CardList from "./CardList";
 
 const Main = () => {
     return (
         <>
         <div>
             <Header></Header>
+            <Banner/>
+            <Search/>
+            <CardList/>
         </div>
         </>
     )
