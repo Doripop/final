33eea2ed16d5f7b3e30798fd161666c6c5c933eb
seongIllee <a01040579861@gmail.com>
2022-07-05 13:17:01 +0900

    상세페이지/홈/메뉴/리뷰_뷰_작업_완료

---
+++ src/components/DetailMenu.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+import { SiBuymeacoffee } from 'react-icons/si';
+import { GiCakeSlice } from 'react-icons/gi';
+
+const DetailMenu = () => {
+    return (
+        <>
+            <Menu>
+                <h1><SiBuymeacoffee/>title1</h1>
+                <div>아이스아메리카노</div>
+                <div>아이스 라떼</div>
+            </Menu>
+            <Menu>
+                <h1><GiCakeSlice/>title2</h1>
+                <div>아이스아메리카노</div>
+                <div>아이스 라떼</div>
+            </Menu>
+        </>
+    );
+}
+
+const Menu = styled.div`
+    width: 1160px;
+    height: 100%;
+
+    margin: 0 auto;
+    padding: 20px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    border-bottom: solid 1px black;
+    
+    & div {
+        width: 350px;
+        height: 100px;
+        border: 1px solid black;
+    }
+`;
+
+export default DetailMenu;
