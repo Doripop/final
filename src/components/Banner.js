52106b4fdb2983e7f87363de7f0ce81e8cbc68b0
seongIllee <a01040579861@gmail.com>
2022-07-29 19:23:53 +0900

    Revert "디테일페이지 게시글보여지는 형식/ 메인배너/ 푸터영역"
    This reverts commit b1fc116e91f296c3c08f4c62f88160aa03c0cbaa.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,24 +6,29 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Button from './Button';
 
-//Banner Img import
-import BannerImg from '../css/BannerImg/service.jpg';
-import BannerImg2 from '../css/BannerImg/survey.jpg';
+import Cafe1 from '../css/cafeImg/cafe1.jpg'
+import Cafe2 from '../css/cafeImg/cafe2.jpg'
+import Cafe3 from '../css/cafeImg/cafe3.jpg'
+import Cafe4 from '../css/cafeImg/cafe4.jpg'
 
 const Banner = () => {
     const settings = {
         infinite: true,
-        speed: 1000,
+        speed: 500,
         slideToShow: 1,
         slideToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000,
+        nextArrow: <Button place="right" margin="0 0 0 590px"/>,
+        prevArrow: <Button margin="0 0 0 -590px"/>,
     };
 
     return (
         <StyledSlider {...settings}>
-            <div><img width={'1400px'} height={'500px'} src={BannerImg} alt='slider' /></div>
-            <div><img width={'1400px'} height={'500px'} src={BannerImg2} alt='slider' /></div>
+            <div><img width={'1900px'} height={'370px'} src={Cafe1} alt='slider' /></div>
+            <div><img width={'1900px'} height={'370px'} src={Cafe2} alt='slider' /></div>
+            <div><img width={'1900px'} height={'370px'} src={Cafe3} alt='slider' /></div>
+            <div><img width={'1900px'} height={'370px'} src={Cafe4} alt='slider' /></div>
         </StyledSlider>
       );
     }
@@ -31,16 +36,18 @@ const Banner = () => {
 const StyledSlider = styled(Slider)`
    //슬라이드 컨테이너 영역
    position: relative;
-   height: 500px; 
-   width: 1400px;
+   height: 370px; 
+   width: 100%;
    margin-bottom: 40px;
    box-sizing: border-box;
 
   .slick-list {  //슬라이드 스크린
-    max-width: 1400px;
-    min-width: 500px;
-    margin-left: 120px;
-    background-size: 50% cover;
+    max-width: 1900px;
+    min-width: 1050px;
+    width: 100%;
+    margin: 0 auto;
+    background-size: cover;
+    background-position: 50% cover;
     background-repeat: no-repeat;
   }
 
--- src/components/DetailPages/DetailReview.js
+++ src/components/DetailPages/DetailReview.js
@@ -261,9 +261,8 @@ const DetailReview = () => {
                             {item.contents}
                         </ReviewContext>
                         <ReviewCommentGroup>
-                        {comment != item.commentList ? (
                             <details>
-                                <summary>댓글 {item.commentCnt}개 모두 보기</summary>
+                                <summary>댓글 모두 보기</summary>
                                 <ReviewComUp>
                                     {item.commentList.map((comment, i) => (
                                         <>
@@ -271,7 +270,7 @@ const DetailReview = () => {
                                                 {userName === comment.nickname ? (
                                                     <span style={{ display: "flex" }}>
                                                         <ReviewProfile src={comment.profileimg} />
-                                                        {comment.nickname}{comment.contents}
+                                                        {comment.nickname} : {comment.contents}
                                                         <Btn style={{ display: click }} onClick={() => { clickevent() }}>🖊</Btn>
                                                         <input
                                                             onChange={(e) => {
@@ -308,7 +307,6 @@ const DetailReview = () => {
                                     ))}
                                 </ReviewComUp>
                             </details>
-                        ):(<summary></summary>)}
                         </ReviewCommentGroup>
                         <ReviewDate>
                             {item.modifiedAt}
@@ -323,25 +321,6 @@ const DetailReview = () => {
                                 onKeyPress={(e) => { keyPress(e, item.postid); }}
                             />
                         </ReviewComment>
-                        <ReviewComUp2>
-                            {item.commentList.map((comment, i) => (
-                                <>
-                                    <div>
-                                        {userName === comment.nickname ? (
-                                            <span style={{ display: "flex", width:264, height: 39, fontSize: "14px"}}>
-                                                <ReviewProfile2 src={comment.profileimg} />
-                                                        {comment.nickname}
-                                                        {comment.contents}
-                                            </span>) : (
-                                            <span style={{ display: "flex" }}><ReviewProfile2 src={comment.profileimg} />{comment.nickname} : {comment.contents}{comment.modifiedAt}
-                                            </span>
-
-                                        )
-                                        }
-                                    </div>
-                                </>
-                            ))}
-                        </ReviewComUp2>
                     </Review>
                 </>
             ))}
@@ -580,47 +559,4 @@ const Btn = styled.button`
         color: red;
     }
 `;
-
-const ReviewComUp2 = styled.div`
-    width: 500px;
-    margin-left: 20px;
-    line-height: 2;
-    color: gray;
-
-    input {
-        position: relative;
-        margin-left: 5px;
-        background-repeat: no-repeat;
-        border: 1px solid #ccc;
-
-        :focus {
-            border-color:#0982f0;
-            outline: none;
-        }
-    }
-`;
-
-const ReviewProfile2 = styled.img`
-    width : 30px;
-    height : 30px;
-    border-radius : 20px;
-    margin: 3px;
-`;
-
-const Btn2 = styled.button`
-    position: relative;
-    /* display: flex; */
-    -webkit-box-align: center;
-    align-items: center;
-    color: black;
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
-    justify-content:center;
-
-    : hover {
-        color: red;
-    }
-`;
-
 export default DetailReview;
